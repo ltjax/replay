d1dc2a93f5f3ea18b99e40a44480c63169b85cb5
ltjax <marius.elvert@googlemail.com>
2019-05-23 00:05:07 +0200

    Update copyright info

--- source/pixbuf_io.cpp
+++ source/pixbuf_io.cpp
@@ -2,7 +2,7 @@
 replay
 Software Library
 
-Copyright (c) 2012 Marius Elvert
+Copyright (c) 2010-2019 Marius Elvert
 
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
