76e67987a7adb451669b2d69cbce8188fa87b4c0
ltjax <marius.elvert@googlemail.com>
2019-06-19 23:04:21 +0200

    Proper options for conan

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile, CMake, tools
+from conans.errors import ConanInvalidConfiguration
 
 
 class ReplayConan(ConanFile):
@@ -10,8 +11,8 @@ class ReplayConan(ConanFile):
     description = "Lean library to observe file changes in a specific directory path."
     topics = ("games", "math")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
+    options = {"shared": [True, False], "use_stb": [True, False], "use_libpng": [True, False]}
+    default_options = {"shared": False, "use_stb": True, "use_libpng": False}
     generators = "cmake"
     exports_sources = "source/*", "test/*", "include/*", "CMakeLists.txt"
     requires = "boost_filesystem/1.69.0@bincrafters/stable", "boost_iostreams/1.69.0@bincrafters/stable",\
@@ -19,8 +20,24 @@ class ReplayConan(ConanFile):
     
     def _configured_cmake(self):
         cmake = CMake(self)
-        cmake.configure(source_folder=".", defs={'Replay_USE_CONAN': 'ON', 'Replay_USE_STBIMAGE': 'OFF', 'Replay_USE_LIBPNG': 'OFF'})
+        cmake.configure(source_folder=".", defs={
+            'Replay_USE_CONAN': True,
+            'Replay_ENABLE_INSTALL': True,
+            'Replay_USE_STBIMAGE': self.options.use_stb,
+            'Replay_USE_STBIMAGE_WRITE': self.options.use_stb,
+            'Replay_USE_STBIMAGE_FROM_CONAN': self.options.use_stb,
+            'Replay_USE_LIBPNG': self.options.use_libpng})
         return cmake
+
+    def configure(self):
+        if self.options.use_libpng and self.options.use_stb:
+            raise ConanInvalidConfiguration("Cannot use both stb and libpng")
+
+    def requirements(self):
+        if self.options.use_stb:
+            self.requires("stb/20190512@conan/stable")
+        if self.options.use_libpng:
+            self.requires("libpng/1.6.37@bincrafters/stable")
 
     def build(self):
         self._configured_cmake().build()
@@ -30,3 +47,5 @@ class ReplayConan(ConanFile):
     
     def package_info(self):
         self.cpp_info.libs = ["replay"]
+        if self.options.use_stb:
+            self.cpp_info.defines.extend(["REPLAY_USE_STBIMAGE", "REPLAY_USE_STBIMAGE_WRITE"])
