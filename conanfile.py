336f3a66306474acc8ee4c1301e3edca3b8a8515
ltjax <marius.elvert@googlemail.com>
2019-12-19 17:03:20 +0100

    Bump version

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans.errors import ConanInvalidConfiguration
 
 class ReplayConan(ConanFile):
     name = "replay"
-    version = "2.0"
+    version = "2.1"
     license = "MIT"
     author = "Marius Elvert marius.elvert@googlemail.com"
     url = "https://github.com/ltjax/replay"
