da267c41a9809cf5889d7b5fe6a66115f2492bfb
ltjax <marius.elvert@googlemail.com>
2020-01-10 00:20:06 +0100

    Improved travis configuration

---
+++ .travis/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+mkdir build && cd build
+conan install .. -s compiler.libcxx=libstdc++11 --build missing
+cmake -DCMAKE_BUILD_TYPE=Release -DReplay_USE_CONAN=ON -DReplay_ENABLE_UNIT_TESTS=OFF -DReplay_USE_STBIMAGE=OFF -DReplay_USE_LIBPNG=OFF ..
+cmake --build .
