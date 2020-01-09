f91c73103beff6dbb02d36ba3587d29aa055ec27
ltjax <marius.elvert@googlemail.com>
2020-01-10 00:38:12 +0100

    Improved travis configuration

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,9 +4,11 @@ set -e
 set -x
 
 uname -s
+sudo apt-get install g++-${GCC_VERSION}
 sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
 sudo update-alternatives --config gcc
 wget https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_20_5.deb
 sudo dpkg -i ./conan-ubuntu-64_1_20_5.deb
 conan --version
 conan config install conan_config/
+conan profile update settings.compiler.libcxx=libstdc++11 default
--- .travis/run.sh
+++ .travis/run.sh
@@ -4,6 +4,6 @@ set -e
 set -x
 
 mkdir build && cd build
-conan install .. -s compiler.libcxx=libstdc++11 --build missing
+conan install .. --build missing
 cmake -DCMAKE_BUILD_TYPE=Release -DReplay_USE_CONAN=ON -DReplay_ENABLE_UNIT_TESTS=OFF -DReplay_USE_STBIMAGE=OFF -DReplay_USE_LIBPNG=OFF ..
 cmake --build .
