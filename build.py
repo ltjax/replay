f062652f1557225462fcefea5f1c11f604858ca0
ltjax <marius.elvert@googlemail.com>
2020-01-10 15:28:29 +0100

    Do not build the shared variant by default

--- build.py
+++ build.py
@@ -3,5 +3,5 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add_common_builds()
+    builder.add_common_builds(shared_option_name=False)
     builder.run()
