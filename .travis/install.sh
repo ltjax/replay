59099bcc05ea2c8a0ba2239b9b661e19a17e8562
ltjax <marius.elvert@googlemail.com>
2020-01-10 01:18:44 +0100

    Do not use virtualenv for macOS

--- .travis/install.sh
+++ .travis/install.sh
@@ -18,17 +18,15 @@ fi
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
     brew outdated pyenv || brew upgrade pyenv
-    brew install pyenv-virtualenv
     brew install cmake || true
 
     if which pyenv > /dev/null; then
         eval "$(pyenv init -)"
     fi
-
+    
+    pyenv versions
     pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
     pyenv rehash
-    pyenv activate conan
 
     pip install conan --upgrade
     pip install conan_package_tools
