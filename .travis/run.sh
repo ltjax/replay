85da6309aa5e252d0448266f759c2b719fdfa72f
ltjax <marius.elvert@googlemail.com>
2020-01-10 16:25:30 +0100

    Use conan package tools on travis

--- .travis/run.sh
+++ .travis/run.sh
@@ -3,7 +3,4 @@
 set -e
 set -x
 
-mkdir build && cd build
-conan install .. --build missing
-cmake -DCMAKE_BUILD_TYPE=Release -DReplay_USE_CONAN=ON -DReplay_ENABLE_UNIT_TESTS=OFF -DReplay_USE_STBIMAGE=OFF -DReplay_USE_LIBPNG=OFF ..
-cmake --build .
+python build.py
