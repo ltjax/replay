b771ef2b18178055cb8c7f191913cf712bad2929
ltjax <marius.elvert@googlemail.com>
2020-01-10 16:34:04 +0100

    Install conan_package_tools on travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,8 +7,9 @@ if [[ "$(uname -s)" == 'Linux' ]]; then
     sudo apt-get install g++-${GCC_VERSION}
     sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-${GCC_VERSION} 60 --slave /usr/bin/g++ g++ /usr/bin/g++-${GCC_VERSION}
     sudo update-alternatives --config gcc
-    wget https://dl.bintray.com/conan/installers/conan-ubuntu-64_1_20_5.deb
-    sudo dpkg -i ./conan-ubuntu-64_1_20_5.deb
+    python3 --version
+    pip3 install conan --upgrade
+    pip3 install conan_package_tools
     conan --version
     conan config install conan_config/
     conan profile new default --detect --force
