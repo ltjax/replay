f9e455a5edcf7372f902ea88738218589e867314
Marius Elvert <marius.elvert@googlemail.com>
2021-11-04 22:30:12 +0100

    Bump replay version to 2.8

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conans.errors import ConanInvalidConfiguration
 
 class ReplayConan(ConanFile):
     name = "replay"
-    version = "2.7"
+    version = "2.8"
     license = "MIT"
     author = "Marius Elvert marius.elvert@googlemail.com"
     url = "https://github.com/ltjax/replay"
